f0ac27a7716e5055da4a1c79b6e6bffe294e0959
arazahari <asgharieharaz@gmail.com>
2024-02-04 10:17:45 +0100

    Create a static web interface

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 FILEPATH = 'todos.txt'
 
+
 def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file:
         todos = file.readlines()
@@ -12,6 +13,8 @@ def write_todos(filepath, todos_arg):
 
 
 print(type(__name__))
-if __name__ == "__main__":
+if __name__ != "__main__":
+    pass
+else:
     print("Hello")
     print(get_todos())
--- gui.py
+++ gui.py
@@ -1,16 +1,20 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 sg.theme('DarkGreen6')
-clock = sg.Text("",key='clock')
+clock = sg.Text("", key='clock')
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter Todo", key="todo")
-add_button = sg.Button(image_source= '004 add.png' , tooltip="Add",
+add_button = sg.Button(image_source='004 add.png', tooltip="Add",
                        mouseover_colors='LightBlue2,', key='Add')
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True,
-                      size=[45, 10])
+                      enable_events=True,size=(40,10))
 complete_button = sg.Button(image_source='004 complete.png',
                             tooltip='Complete',
                             mouseover_colors='LightBlue2',
@@ -21,7 +25,9 @@ layout = ([clock], [label], [input_box, add_button],
           [list_box, edit_button, complete_button],
           [exit_button])
 window = sg.Window("My To-Do App",
-                   layout = layout,
+                   layout=[[clock], [label], [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 15))
 
 while True:
@@ -30,6 +36,7 @@ while True:
     print(event)
     print(values)
     print(values['todos'])
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -57,8 +64,8 @@ while True:
                 todos = functions.get_todos()
                 todos.remove(todo_to_complete)
                 functions.write_todos('todos.txt', todos)
-                window["todos"].update(values= todos)
-                window["todo"].update(value= '')
+                window["todos"].update(values=todos)
+                window["todo"].update(value='')
             except IndexError:
                 sg.popup("Please Select Something")
         case 'Exit':
